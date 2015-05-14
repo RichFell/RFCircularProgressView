173d048bc20c664c429aa18bbd86d130a2ed75ea
Rich Fellure <rfellure24@gmail.com>
2015-05-14 15:50:29 -0700

    Initial Commit

---
+++ RFCircularProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RFCircularProgress
+//
+//  Created by Rich Fellure on 5/14/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RFCircularProgress/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RFCircularProgress
+//
+//  Created by Rich Fellure on 5/14/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
