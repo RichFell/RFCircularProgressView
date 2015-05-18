681e28d310560845fdc878d6e5380fed21603220
Rich Fellure <rfellure24@gmail.com>
2015-05-18 12:18:53 -0700

    Reverted back and made some fixes

--- RFCircularProgress/RFProgressView.m
+++ RFCircularProgress/RFProgressView.m
@@ -57,33 +57,29 @@
 
     float circWidth = self.circleWidth ? self.circleWidth : 1.0;
 
-    if (!circlePath) {
-        circlePath = [UIBezierPath bezierPath];
+    circlePath = circlePath ? circlePath : [UIBezierPath bezierPath];
 
-        [circlePath addArcWithCenter:CGPointMake(CGRectGetWidth(self.frame)/2,
+    [circlePath addArcWithCenter:CGPointMake(CGRectGetWidth(self.frame)/2,
                                              CGRectGetHeight(self.frame)/2)
-                          radius:CGRectGetWidth(self.frame)/2 - circWidth
+                          radius:CGRectGetWidth(self.frame)/2 - circWidth/2
                       startAngle:startAngle
                         endAngle:endAngle
                        clockwise:YES];
-    }
 
-    if (!progressLayer) {
-        progressLayer = [[CAShapeLayer alloc] init];
+    progressLayer = progressLayer ? progressLayer : [[CAShapeLayer alloc] init];
 
-        [progressLayer setPath: circlePath.CGPath];
+    [progressLayer setPath: circlePath.CGPath];
 
-        [progressLayer setStrokeColor:self.circleColor.CGColor];
-        [progressLayer setFillColor:[UIColor clearColor].CGColor];
-        [progressLayer setLineWidth:self.circleWidth];
+    [progressLayer setStrokeColor:self.circleColor.CGColor];
+    [progressLayer setFillColor:[UIColor clearColor].CGColor];
+    [progressLayer setLineWidth:self.circleWidth];
 
-        [progressLayer setStrokeStart:0.0];
+    [progressLayer setStrokeStart:0.0];
 
-        CGFloat strokeEnd = self.percent > 1.0 ? self.percent/100 : self.percent;
-        [progressLayer setStrokeEnd:strokeEnd];
+    CGFloat strokeEnd = self.percent > 1.0 ? self.percent/100 : self.percent;
+    [progressLayer setStrokeEnd:strokeEnd];
 
-        [self.layer addSublayer:progressLayer];
-    }
+    [self.layer addSublayer:progressLayer];
 
     //If set to show the insetCircle, then we want to dislay it
     if (!self.hidesInsetCircle) {
@@ -97,7 +93,7 @@
         }
         [insetCirclePath addArcWithCenter:CGPointMake(CGRectGetWidth(self.frame)/2,
                                                  CGRectGetHeight(self.frame)/2)
-                              radius:CGRectGetWidth(self.frame)/2 - circWidth - insetCircWidth/2
+                              radius:CGRectGetWidth(self.frame)/2 - circWidth/2 - insetCircWidth/2
                           startAngle:startAngle
                             endAngle:endAngle
                            clockwise:YES];
