d6d7e617d4438d0ed9dca073fa0d5a22878f0ad0
Rich Fellure <rfellure24@gmail.com>
2015-05-18 11:47:05 -0700

    Circle clips better to the boundary it is in.

--- RFCircularProgress/RFProgressView.m
+++ RFCircularProgress/RFProgressView.m
@@ -53,37 +53,33 @@
 -(void)drawCircle {
     //Have to adjust to multiplier according to the percentage given
     startAngle = M_PI * 1.5;
-    endAngle = startAngle + (M_PI * 2);
+    endAngle = startAngle + (M_PI * ((self.percent/100) * 2));
 
     float circWidth = self.circleWidth ? self.circleWidth : 1.0;
 
-    if (!circlePath) {
-        circlePath = [UIBezierPath bezierPath];
+    circlePath = [UIBezierPath bezierPath];
 
-        [circlePath addArcWithCenter:CGPointMake(CGRectGetWidth(self.frame)/2,
-                                             CGRectGetHeight(self.frame)/2)
-                          radius:CGRectGetWidth(self.frame)/2 - circWidth
+    [circlePath addArcWithCenter:CGPointMake(CGRectGetWidth(self.frame)/2,
+                                         CGRectGetHeight(self.frame)/2)
+                      radius:CGRectGetWidth(self.frame)/2 - circWidth/2
                       startAngle:startAngle
                         endAngle:endAngle
                        clockwise:YES];
-    }
 
-    if (!progressLayer) {
-        progressLayer = [[CAShapeLayer alloc] init];
+    progressLayer = [[CAShapeLayer alloc] init];
 
-        [progressLayer setPath: circlePath.CGPath];
+    [progressLayer setPath: circlePath.CGPath];
 
-        [progressLayer setStrokeColor:self.circleColor.CGColor];
-        [progressLayer setFillColor:[UIColor clearColor].CGColor];
-        [progressLayer setLineWidth:self.circleWidth];
+    [progressLayer setStrokeColor:self.circleColor.CGColor];
+    [progressLayer setFillColor:[UIColor clearColor].CGColor];
+    [progressLayer setLineWidth:self.circleWidth];
 
-        [progressLayer setStrokeStart:0.0];
+    [progressLayer setStrokeStart:0.0];
 
-        CGFloat strokeEnd = self.percent > 1.0 ? self.percent/100 : self.percent;
-        [progressLayer setStrokeEnd:strokeEnd];
+    CGFloat strokeEnd = self.percent > 1.0 ? self.percent/100 : self.percent;
+    [progressLayer setStrokeEnd:strokeEnd];
 
-        [self.layer addSublayer:progressLayer];
-    }
+    [self.layer addSublayer:progressLayer];
 
     //If set to show the insetCircle, then we want to dislay it
     if (!self.hidesInsetCircle) {
@@ -97,7 +93,7 @@
         }
         [insetCirclePath addArcWithCenter:CGPointMake(CGRectGetWidth(self.frame)/2,
                                                  CGRectGetHeight(self.frame)/2)
-                              radius:CGRectGetWidth(self.frame)/2 - circWidth - insetCircWidth/2
+                              radius:CGRectGetWidth(self.frame)/2 - circWidth/2 - insetCircWidth/2
                           startAngle:startAngle
                             endAngle:endAngle
                            clockwise:YES];
@@ -130,13 +126,13 @@
 }
 
 /**
- Description: Animates the Progress bar to the passed in percent
+ Description: Animates the Progress bar to the given percent
 
  :newPecent: New percent to move the progress bar to from scale of 0.0 to 1.0
  */
 -(void)changePercent:(CGFloat)numerator byDenominator:(CGFloat)denominator{
     CABasicAnimation *animateStrokeEnd = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    animateStrokeEnd.duration  = numerator == denominator ? 0.0 : 0.1;
+    animateStrokeEnd.duration  = numerator == denominator ? 0.0 : 0.5;
     animateStrokeEnd.fromValue = @(self.percent);
     animateStrokeEnd.toValue   = [NSNumber numberWithFloat:numerator / denominator];
     animateStrokeEnd.fillMode = kCAFillModeBoth;
